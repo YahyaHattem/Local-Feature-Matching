e2a5c2e58cf845d33b5c2c9d77e7ce0578bcae5a
AhmedElghamry <37878392+AhmedElghamry@users.noreply.github.com>
2023-04-08 14:54:49 +0200

    Fin

--- code/student.py
+++ code/student.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.filters import sobel_h, sobel_v, gaussian
 
 
 def get_interest_points(image, feature_width):
@@ -43,12 +44,47 @@ def get_interest_points(image, feature_width):
     """
 
     # TODO: Your implementation here! See block comments and the project webpage for instructions
+    alpha = 0.06
+    threshold = 0.01
+    stride = 2
+    sigma = 0.1
+    sigma0 = 0.1
 
     # These are placeholders - replace with the coordinates of your interest points!
-    xs = np.zeros(1)
-    ys = np.zeros(1)
+    xs = list()
+    ys = list()
 
-    return xs, ys
+    # step1: blur image
+    filtered_image = gaussian(image, sigma=sigma)
+
+    # step2: calculate gradient of image
+    G_x = sobel_v(filtered_image)
+    G_y = sobel_h(filtered_image)
+
+    # step3: calculate Gxx, Gxy, Gyy
+    G_xx = gaussian(np.square(G_x), sigma=sigma0)
+    G_xy = gaussian(np.multiply(G_x, G_y), sigma=sigma0)
+    G_yy = gaussian(np.square(G_y), sigma=sigma0)
+
+    # step4: calculate C matrix
+    for y in range(0, image.shape[0] - feature_width, stride):
+        for x in range(0, image.shape[1] - feature_width, stride):
+            Sxx = np.sum(G_xx[y:y + feature_width +
+                         1, x:x + feature_width + 1])
+            Syy = np.sum(G_yy[y:y + feature_width +
+                         1, x:x + feature_width + 1])
+            Sxy = np.sum(G_xy[y:y + feature_width +
+                         1, x:x + feature_width + 1])
+
+            detC = (Sxx * Syy) - (Sxy ** 2)
+            traceC = Sxx + Syy
+            C = detC - alpha * (traceC ** 2)
+
+            if C > threshold:
+                xs.append(x + int(feature_width / 2 - 1))
+                ys.append(y + int(feature_width / 2 - 1))
+
+    return np.asarray(xs), np.asarray(ys)
 
 
 def get_features(image, x, y, feature_width):
@@ -114,9 +150,77 @@ def get_features(image, x, y, feature_width):
     """
 
     # TODO: Your implementation here! See block comments and the project webpage for instructions
+    sigma_gradient_image = 0.1
+    sigma_16x16 = 0.4
+    threshold = 0.2
 
     # This is a placeholder - replace this with your features!
-    features = np.zeros((1, 128))
+    features = np.zeros((len(x), 4, 4, 8))
+
+    # step0: blur image
+    filtered_image = gaussian(image, sigma=sigma_gradient_image)
+
+    # step1: compute the gradient of image
+    G_x = sobel_v(filtered_image)
+    G_y = sobel_h(filtered_image)
+
+    magnitude_gradient = np.sqrt(np.add(np.square(G_x), np.square(G_y)))
+    direction_gradient = np.arctan2(G_y, G_x)
+    direction_gradient[direction_gradient < 0] += 2 * np.pi
+
+    # step2:
+    # image.shape[0] = 1024
+    # image.shape[1] = 768
+
+    for n, (x_, y_) in enumerate(zip(x, y)):
+        rows = (y_ - feature_width // 2, y_ + feature_width // 2 + 1)
+        cols = (x_ - feature_width // 2, x_ + feature_width // 2 + 1)
+
+        if rows[0] < 0:
+            rows = (0, feature_width + 1)
+        if rows[1] > image.shape[0]:
+            rows = (image.shape[0] - feature_width - 1, image.shape[0] - 1)
+
+        if cols[0] < 0:
+            cols = (0, feature_width + 1)
+        if cols[1] > image.shape[1]:
+            cols = (image.shape[1] - feature_width - 1, image.shape[1] - 1)
+
+    # step3: get gradient and angle of key point
+    magnitude_window = magnitude_gradient[rows[0]:rows[1], cols[0]:cols[1]]
+    direction_window = direction_gradient[rows[0]:rows[1], cols[0]:cols[1]]
+
+    # step4: Gaussian filter on window
+    magnitude_window = gaussian(magnitude_window, sigma=sigma_16x16)
+    direction_window = gaussian(direction_window, sigma=sigma_16x16)
+
+    for i in range(feature_width // 4):
+        for j in range(feature_width // 4):
+            current_magnitude = magnitude_window[i * feature_width // 4: (
+                i + 1) * feature_width // 4, j * feature_width // 4:(j + 1) * feature_width // 4]
+
+            current_direction = direction_window[i * feature_width // 4: (
+                i + 1) * feature_width // 4, j * feature_width // 4:(j + 1) * feature_width // 4]
+
+            features[n, i, j] = np.histogram(current_direction.reshape(-1), bins=8, range=(
+                0, 2 * np.pi), weights=current_magnitude.reshape(-1))[0]
+
+    # step5: finalize the output
+    # 1. 128-dim vector
+    features = features.reshape((len(x), -1,))
+
+    # 2. Normalization
+    norm = np.sqrt(np.square(features).sum(axis=1)).reshape(-1, 1)
+    norm[norm == 0] = 1
+    features = features / norm
+
+    # 3. Clamp all vector values > 0.2
+    features[features >= threshold] = threshold
+
+    # 4. re-normalize
+    norm = np.sqrt(np.square(features).sum(axis=1)).reshape(-1, 1)
+    norm[norm == 0] = 1
+    features = features / norm
 
     return features
 
@@ -153,10 +257,20 @@ def match_features(im1_features, im2_features):
     """
 
     # TODO: Your implementation here! See block comments and the project webpage for instructions
+    threshold = 0.8
 
     # These are placeholders - replace with your matches and confidences!
+    matches = list()
+    confidences = list()
+    print(im1_features.shape[0])
+    for i in range(im1_features.shape[0]):
+        distances = np.sqrt(np.square(np.subtract(
+            im1_features[i, :], im2_features)).sum(axis=1))
+        index_sorted = np.argsort(distances)
+        print(i, index_sorted)
+        if distances[index_sorted[0]] / distances[index_sorted[1]] < threshold:
+            matches.append([i, index_sorted[0]])
+            confidences.append(
+                1.0 - distances[index_sorted[0]] / distances[index_sorted[1]])
 
-    matches = np.zeros((1, 2))
-    confidences = np.zeros(1)
-
-    return matches, confidences
+    return np.asarray(matches), np.asarray(confidences)
